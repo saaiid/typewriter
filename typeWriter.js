f88503c096cb7282a85b7ce52ae6306137d97df1
saaiid <118833462+saaiid@users.noreply.github.com>
2023-01-18 16:41:11 -0500

    Create typeWriter.js
    Animated Sentence

---
+++ typeWriter.js
@@ -0,0 +1,9 @@
+const sentence = "hello there from lighthouse labs";
+let delay = 0;
+
+for (const char of sentence) {
+  setTimeout(() => {
+    process.stdout.write(char);
+  }, delay);
+  delay += 50;
+}
